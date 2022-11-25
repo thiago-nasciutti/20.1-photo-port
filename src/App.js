3c8fdfea67336d8d491918b5833f2894202d774e
thiago <nasciutti.thiago@gmail.com>
2022-11-25 12:01:31 -0500

    about-nav

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
+//'App.js' file is the center of the application = wrap all components
+
+//React needs to be imported in every component file
+import React from 'react';
 import './App.css';
+import About from './components/About';
+import Nav from './components/Nav';
 
 function App() {
+  //returns JSX that represents HTML in JavaScript
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //React components must always return a single parente JSX element. However, this element may have many children elements
+    <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
